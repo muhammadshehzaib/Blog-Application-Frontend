463bad029844daf33533eaa2565d1cd8d4fd7d04
m-Shehzaib <98905825+muhammadshehzaib@users.noreply.github.com>
2024-02-26 13:13:44 +0500

    Fix admin link in the Admin Panel component

--- components/blogs/Adminpanel.tsx
+++ components/blogs/Adminpanel.tsx
@@ -19,7 +19,7 @@ const AdminPanel = () => {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          "https://blog-application-two-psi.vercel.app//blogs"
+          "https://blog-application-two-psi.vercel.app/blogs"
         );
         if (!response.ok) {
           throw new Error("Failed to fetch blog data");
